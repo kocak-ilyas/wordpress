8f8c0be63337f696bb6b35b9e6fea7d7c9865afd
ilyas kocak <kocakilyas18@gmail.com>
2021-07-02 11:45:27 +0300

    followAndrew done

---
+++ wp-content/themes/follwandrew/comments.php
@@ -0,0 +1,37 @@
+<div class="comments-wrapper">
+    <div class="comments" id="comments">
+        <div class="comments-header">
+            <h2 class="comment-reply-title">
+                <?php
+                if (!have_comments()) {
+                    echo "Leave a Comment";
+                } else {
+                    echo get_comments_number() . "Comments";
+                }
+                ?>
+            </h2>
+        </div>
+        <div class="comments-inner">
+            <?php
+            wp_list_comments(
+                array(
+                    "avatar_size" => 120,
+                    "style" => "div",
+                )
+            );
+            ?>
+        </div>
+    </div>
+    <hr class="" aria-hidden="true">
+    <?php
+    if (comments_open()) {
+        comment_form(
+            array(
+                "class_form" => "",
+                "title_reply_before" => '<h2 id="reply-title" class="comment-reply-title">',
+                "title_reply_afer" => "</h2>"
+            )
+        );
+    }
+    ?>
+</div>
--- wp-content/themes/follwandrew/functions.php
+++ wp-content/themes/follwandrew/functions.php
@@ -1,8 +1,62 @@
-<?php function followandrew_register_styles(){
-    wp_enqueue_style("followandrew-style", get_template_directory_uri()."/style.css",array(),"1.0","all");
-    wp_enqueue_style("followandrew-bootstrap","https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",array(),"1.0","all");
-    wp_enqueue_style("followandrew-fontawesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(),"1.0","all");
+<?php 
+function followandrew_theme_support(){
+    // adds dynamic title tag support
+    add_theme_support("title-tag"); 
+    add_theme_support("custom-logo"); 
+    add_theme_support("post-thumbnails"); 
 }
+add_action("after_setup_theme","followandrew_theme_support");
 
-add_action("wp_enqueue_script","followandrew_register_styles")
-?>
+function followandrew_menus(){
+    $locations=array(
+    "primary"=>"Desktop Primary Left Sidebar",
+    "footer"=>"Footer Menu Items"
+    );
+    register_nav_menus($locations);
+}
+add_action("init","followandrew_menus");
+
+function followandrew_register_styles(){
+    $version=wp_get_theme()->get("Version");
+    wp_enqueue_style("style", get_stylesheet_uri());
+    // wp_enqueue_style("followandrew-style", get_template_directory_uri() . "/style.css", array("followandrew-bootstrap"), $version, "all");
+    wp_enqueue_style("followandrew-bootstrap","https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",array(),"4.4.1","all");
+    wp_enqueue_style("followandrew-fontawesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(),"5.13.0","all");
+}
+add_action("wp_enqueue_script","followandrew_register_styles");
+
+function followandrew_register_scripts(){
+    wp_enqueue_script("followandrew-jquery","https://code.jquery.com/jquery-3.4.1.slim.min.js",array(),"3.4.1",true);
+    wp_enqueue_script("followandrew-popper","https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",array(),"1.16.0",true);
+    wp_enqueue_script("followandrew-bootstrap",get_template_directory_uri() ."/assets/js/main.js",array(),"1.0",true);
+    wp_enqueue_script("followandrew-main","https://code.jquery.com/jquery-3.4.1.slim.min.js",array(),"3.4.1",true);
+}
+add_action("wp_enqueue_script","followandrew_register_scripts");
+
+
+function followandrew_widget_areas(){
+    register_sidebar( 
+        array(
+            'before_title' => "",
+            "after_title"=>"",
+            "before_widget"=>'<ul class="social-list list-inline py-3 mx-auto">',
+            "after_widget"=>"</ul>",     
+            "name"=>"Sidebar area",
+            "id"=>"sidebar-1",
+            "description"=>"Sidebar Widget Area"
+        )
+     );
+
+    register_sidebar( 
+        array(
+            'before_title' => "",
+            "after_title"=>"",
+            "before_widget"=>'<ul class="social-list list-inline py-3 mx-auto">',
+            "after_widget"=>"</ul>",
+            "name"=>"Footer area",
+            "id"=>"footer-1",
+            "description"=>"Footer Widget Area"
+        )
+     );
+}
+add_action("widget_init","followandrew_widget_areas");
