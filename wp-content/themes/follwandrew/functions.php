feaf7959ee3580fc6ca42908684b1c2572a8ab31
ilyas kocak <kocakilyas18@gmail.com>
2021-07-02 18:20:43 +0300

    css ok - js none

--- wp-content/themes/follwandrew/functions.php
+++ wp-content/themes/follwandrew/functions.php
@@ -18,20 +18,24 @@ function followandrew_menus()
 }
 add_action("init", "followandrew_menus");
 
-function followandrew_register_styles(){
-    $version=wp_get_theme()->get("Version");
+function followandrew_register_styles()
+{
+    $version = wp_get_theme()->get("Version");
     wp_enqueue_style("followandrew-style", get_stylesheet_uri(), array("followandrew-bootstrap"), $version, "all");
-    wp_enqueue_style("followandrew-bootstrap","https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",array(),"4.4.1","all");
-    wp_enqueue_style("followandrew-fontawesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(),"5.13.0","all");
+    wp_enqueue_style("followandrew-bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), "4.4.1", "all");
+    wp_enqueue_style("followandrew-fontawesome", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), "5.13.0", "all");
 }
-add_action( 'wp_enqueue_scripts', 'followandrew_register_styles' );
+add_action('wp_enqueue_scripts', 'followandrew_register_styles');
 
 function followandrew_register_scripts()
 {
     wp_enqueue_script("followandrew-jquery", "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), "3.4.1", true);
+
     wp_enqueue_script("followandrew-popper", "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), "1.16.0", true);
-    wp_enqueue_script("followandrew-bootstrap", get_template_directory_uri() . "/assets/js/main.js", array(), "1.0", true);
-    wp_enqueue_script("followandrew-main", "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), "3.4.1", true);
+
+    wp_enqueue_script("followandrew-bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js", array(), "5.0.2", true);
+
+    wp_enqueue_script("followandrew-main", get_template_directory_uri() . "/assets/js/main.js", array(), "1.0", true);
 }
 add_action("wp_enqueue_script", "followandrew_register_scripts");
 
